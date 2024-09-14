f2609799759d5c7d9ed210045d13fdf9071e89e5
Oleh <olegsuv.dev@gmail.com>
2024-09-15 00:07:32 +0300

    feat: basics of styling tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,29 @@
 import { Tabs } from "expo-router";
-
 import Octicons from "@expo/vector-icons/Octicons";
 import Feather from "@expo/vector-icons/Feather";
 import Ionicons from "@expo/vector-icons/Ionicons";
-
 import { useColorScheme } from "react-native";
+import tailwindConfig from "../../tailwind.config";
 
 export default function TabLayout() {
   const userTheme = useColorScheme();
 
+  // @ts-ignore
+  const darkColor = tailwindConfig.theme.extend.colors.darkColor;
+  // @ts-ignore
+  const sampleColor = tailwindConfig.theme.extend.colors.sampleColor;
+
   return (
-    <Tabs screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: userTheme === "dark" ? darkColor : "white",
+          borderColor: sampleColor,
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
