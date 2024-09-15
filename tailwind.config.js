1434113003faf352d25c15de517051ab6c8461b6
Oleh <olegsuv.dev@gmail.com>
2024-09-15 11:53:35 +0300

    chore: correct tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./index.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       // color theme: https://designs.ai/colors/palette/19
