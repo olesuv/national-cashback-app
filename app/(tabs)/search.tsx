e3abcf6214730abcb84bfbf53907508f6952b172
Oleh <olegsuv.dev@gmail.com>
2024-09-17 22:11:23 +0300

    feat: search with good error display (ui)

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -9,18 +9,31 @@ import {
   SafeAreaView,
   NativeSyntheticEvent,
   TextInputChangeEventData,
+  Linking,
 } from "react-native";
 import { Octicons } from "@expo/vector-icons";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import axios from "axios";
+
+export interface ProductDTO {
+  barcode: string;
+  brand: string;
+  product_name: string;
+  legal_name: string;
+}
+
+interface ProductErrorDTO {
+  message: string;
+  error: string;
+  statusCode: number;
+}
 
 export default function SearchScreen() {
   const [searchQuery, setSearchQuery] = React.useState("");
+  const [searchError, setSearchError] = React.useState("");
+  const [searchResults, setSearchResults] = React.useState<ProductDTO[]>([]);
 
   const handleScroll = () => {
-    Keyboard.dismiss();
-  };
-
-  const handleDonePress = () => {
     Keyboard.dismiss();
   };
 
@@ -32,9 +45,28 @@ export default function SearchScreen() {
     setSearchQuery("");
   };
 
+  const handleDonePress = async () => {
+    Keyboard.dismiss();
+
+    try {
+      const res = await handleSearchQuery(searchQuery);
+      if (Array.isArray(res)) {
+        setSearchResults(res);
+        setSearchError("");
+      }
+    } catch (err: any) {
+      if (err?.statusCode === 404 && err?.error === "Not Found") {
+        setSearchError("Нічого не було знайдено по твому запиту 😞");
+      } else {
+        setSearchError("⚙️ Невідома помилка. Її хтось вже виправляє...");
+        console.error(err);
+      }
+    }
+  };
+
   return (
     <View style={tw`flex-1`}>
-      <SafeAreaView style={tw` bg-zinc-50 shadow-md`}>
+      <SafeAreaView style={tw`bg-zinc-50 shadow-md`}>
         <View style={tw`px-4 py-2`}>
           <View style={tw`flex-row items-center rounded-xl border border-zinc-300 bg-white p-1`}>
             <TextInput
@@ -55,20 +87,59 @@ export default function SearchScreen() {
         </View>
       </SafeAreaView>
 
-      <ScrollView
-        contentContainerStyle={tw`flex-grow p-4`}
-        keyboardShouldPersistTaps="handled"
-        onScrollBeginDrag={handleScroll}
-      >
-        <Text style={tw`mb-4 text-center`}>Search results will appear here</Text>
-
-        {/* Sample content */}
-        {[...Array(20)].map((_, index) => (
-          <Text key={index} style={tw`mt-4`}>
-            Sample search result item {index + 1}
-          </Text>
-        ))}
-      </ScrollView>
+      {searchError !== "" ? (
+        <View style={tw`flex-1 items-center justify-center bg-transparent px-4`}>
+          <View style={tw`rounded-xl bg-rose-500 p-4`}>
+            <Text style={tw`rounded pb-3 text-center text-2xl font-semibold text-white`}>{searchError}</Text>
+            <View style={tw`flex-col items-start rounded-xl border border-rose-300 bg-rose-400 p-2`}>
+              <Text style={tw`pb-2 text-white`}>
+                - Не забувай, що не всі товари підпадають під програму "Національний Кешбек"
+                <Text
+                  style={tw`text-rose-200 underline`}
+                  onPress={() => Linking.openURL("https://madeinukraine.gov.ua/")}
+                >
+                  (дитись тут)
+                </Text>
+                {"."}
+              </Text>
+              <Text style={tw`pb-2 text-white`}>
+                - Товари оновлюються автоматично щотижня, тому можливо вони з'являться.
+              </Text>
+              <Text style={tw`text-white`}>
+                - Додаток <Text style={tw`underline`}>не</Text> є офіційним.
+              </Text>
+            </View>
+          </View>
+        </View>
+      ) : (
+        <ScrollView
+          contentContainerStyle={tw`flex-grow p-4`}
+          keyboardShouldPersistTaps="handled"
+          onScrollBeginDrag={handleScroll}
+        >
+          {searchResults.map((product, index) => (
+            <View key={index} style={tw`border-b border-gray-200 p-4`}>
+              <Text>{product.product_name}</Text>
+              <Text>{product.brand}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      )}
     </View>
   );
+}
+
+async function handleSearchQuery(searchProductName: string): Promise<ProductDTO[]> {
+  return await axios
+    .get(`${process.env.EXPO_PUBLIC_API}/products/search-name`, {
+      params: {
+        name: searchProductName,
+      },
+    })
+    .then((response) => {
+      return response.data as ProductDTO[];
+    })
+    .catch((error) => {
+      throw error.response.data as ProductErrorDTO;
+    });
 }
