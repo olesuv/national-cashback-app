3b2bbc415cd24b897588d5e77e6bf0d08f8c29fa
Oleh <olegsuv.dev@gmail.com>
2024-09-18 12:47:56 +0300

    refactor(style): search pages to component

---
+++ components/search/SearchPages.tsx
@@ -0,0 +1,37 @@
+import { Text, TouchableOpacity, View } from "react-native";
+
+interface SearchPagesProps {
+  offset: number;
+  limit: number;
+  searchResults: any[];
+  handlePrevPage: () => void;
+  handleNextPage: () => void;
+}
+
+export default function SearchPages({
+  offset,
+  limit,
+  searchResults,
+  handlePrevPage,
+  handleNextPage,
+}: SearchPagesProps) {
+  return (
+    <View style={tw`mb-20 flex-row items-center justify-between border-zinc-200 bg-transparent p-4`}>
+      <TouchableOpacity
+        onPress={handlePrevPage}
+        disabled={offset === 0}
+        style={tw`rounded-lg bg-blue-500 px-4 py-2 ${offset === 0 ? "opacity-50" : ""}`}
+      >
+        <Text style={tw`font-semibold text-white`}>Попередня</Text>
+      </TouchableOpacity>
+      <Text style={tw`text-zinc-600`}>{`Сторінка ${Math.floor(offset / limit) + 1}`}</Text>
+      <TouchableOpacity
+        onPress={handleNextPage}
+        disabled={searchResults.length < limit}
+        style={tw`rounded-lg bg-blue-500 px-4 py-2 ${searchResults.length < limit ? "opacity-50" : ""}`}
+      >
+        <Text style={tw`font-semibold text-white`}>Наступна</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
--- components/search/SearchReuslts.tsx
+++ components/search/SearchReuslts.tsx
@@ -1,5 +1,6 @@
 import { ProductDTO } from "@/app/(tabs)/search";
 import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import SearchPages from "./SearchPages";
 
 interface SearchResultsProps {
   searchResults: ProductDTO[];
@@ -41,23 +42,13 @@ export default function SearchResults({
         ))}
 
         {searchResults.length > 0 && (
-          <View style={tw`mb-20 flex-row items-center justify-between border-zinc-200 bg-transparent p-4`}>
-            <TouchableOpacity
-              onPress={handlePrevPage}
-              disabled={offset === 0}
-              style={tw`rounded-lg bg-blue-500 px-4 py-2 ${offset === 0 ? "opacity-50" : ""}`}
-            >
-              <Text style={tw`font-semibold text-white`}>Попередня</Text>
-            </TouchableOpacity>
-            <Text style={tw`text-zinc-600`}>{`Сторінка ${Math.floor(offset / limit) + 1}`}</Text>
-            <TouchableOpacity
-              onPress={handleNextPage}
-              disabled={searchResults.length < limit}
-              style={tw`rounded-lg bg-blue-500 px-4 py-2 ${searchResults.length < limit ? "opacity-50" : ""}`}
-            >
-              <Text style={tw`font-semibold text-white`}>Наступна</Text>
-            </TouchableOpacity>
-          </View>
+          <SearchPages
+            offset={offset}
+            limit={limit}
+            searchResults={searchResults}
+            handlePrevPage={handlePrevPage}
+            handleNextPage={handleNextPage}
+          />
         )}
       </ScrollView>
     </View>
