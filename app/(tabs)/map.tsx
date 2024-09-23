517637eaaa9c0489e8c97cd5d7c5ba9bd2e11390
Oleh <olegsuv.dev@gmail.com>
2024-09-23 16:21:19 +0300

    chore: map init text add

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import MapView from "react-native-maps";
-import { View } from "react-native";
+// import MapView from "react-native-maps";
+import { Text, View } from "react-native";
 
 export default function MapScreen() {
   return (
     <View style={tw`flex-1 items-center justify-center bg-inherit`}>
-      <MapView />
+      {/* <MapView /> */}
+      <Text>here sometime will be map view of all available sellers' points</Text>
     </View>
   );
 }
