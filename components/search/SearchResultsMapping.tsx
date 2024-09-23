9b5621f25849296dfe85e3908b4a7e4185c145d2
Oleh <olegsuv.dev@gmail.com>
2024-09-23 17:51:22 +0300

    style: product search results

--- components/search/SearchResultsMapping.tsx
+++ components/search/SearchResultsMapping.tsx
@@ -17,11 +17,13 @@ export default function SearchResultsMapping({ product, index }: ProductMappingP
   };
 
   return (
-    <View key={index} style={tw`mb-2 rounded-lg border border-zinc-300 bg-white p-4`}>
+    <View key={index} style={tw`mb-2 rounded-xl border border-zinc-300 bg-white p-4`}>
       <TouchableOpacity onPress={toggleModal}>
-        <Text style={tw`text-base font-semibold text-black`}>{product.product_name}</Text>
-        <Text style={tw`text-sm text-zinc-500`}>({product.barcode})</Text>
-        <Text style={tw`text-sm text-zinc-500`}>{product.brand}</Text>
+        <Text style={tw`mb-2  text-lg text-black`}>
+          <Text style={tw`font-semibold`}>{product.product_name} </Text>
+          <Text style={tw`text-sm text-zinc-500`}>({product.barcode})</Text>
+        </Text>
+        <Text style={tw`text-base text-zinc-500`}>{product.brand}</Text>
       </TouchableOpacity>
       <FullInfoPopUp infoData={product} isModalVisible={isModalVisible} toggleModal={toggleModal} />
     </View>
